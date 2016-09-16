7f96321db00b8937fa8b3e92083f8ad77e8d940c
Oleg Kunov <oleg.kunov@gmail.com>
2016-09-16 16:19:25 +0300

    continuing secondary attributes; refactoring

--- cli.rb
+++ cli.rb
@@ -14,25 +14,45 @@ module CLI
     end
   end
 
+  def attr_change(hc, input, attr_dir)
+    attr = attr_dir[:attr]
+    length = if attr_dir[:len] then attr_dir[:len] else 2 end
+    # coeff = if attr_dir[:dir] == :plus then 1; else -1 end
+    levels = input[length..-1]
+    levels =
+        case
+          when levels == '+'
+            1
+          when levels == '-'
+            -1
+          else
+            levels.to_i
+        end
+    hc.attr_change(attr, levels)
+  end
+
   def basic_attributes(hc)
+
     continue = true
     while continue
       p hc.effective_stats
       puts "#{hc.points} points"
       puts 'Basic attributes: <st>, <dx>, <iq>, <ht>. <q> to exit'
-      input = gets
-      case input.chop
-        when 'q' then continue = false
-        when 'st+' then hc.attr_change(:st, 1)
-        when 'st-' then hc.attr_change(:st, -1)
-        when 'dx+' then hc.attr_change(:dx, 1)
-        when 'dx-' then hc.attr_change(:dx, -1)
-        when 'iq+' then hc.attr_change(:iq, 1)
-        when 'iq-' then hc.attr_change(:iq, -1)
-        when 'ht+' then hc.attr_change(:ht, 1)
-        when 'ht-' then hc.attr_change(:ht, -1)
-        else
-      end
+      input = gets.chop
+      attr_dir =
+          case input
+            when 'q' then continue = false; nil
+            when /^st\+/ then { attr: :st, dir: :plus }
+            when /^st\-/ then { attr: :st, dir: :minus }
+            when /^dx\+/ then { attr: :dx, dir: :plus }
+            when /^dx\-/ then { attr: :dx, dir: :minus }
+            when /^iq\+/ then { attr: :iq, dir: :plus }
+            when /^iq\-/ then { attr: :iq, dir: :minus }
+            when /^ht\+/ then { attr: :ht, dir: :plus }
+            when /^ht\-/ then { attr: :ht, dir: :minus }
+            else
+          end
+      attr_change(hc, input, attr_dir) unless attr_dir.nil?
     end
   end
 
@@ -41,16 +61,20 @@ module CLI
     while continue
       p hc.effective_stats
       puts "#{hc.points} points"
-      puts 'Secondary attributes: <hp>. <q> to exit'
-      input = gets
-      case input.chop
-        when 'q' then continue = false
-        when 'hp+' then hc.attr_change(:hp, 1)
-        when 'hp-' then hc.attr_change(:hp, -1)
-        when 'wl+' then hc.attr_change(:wl, 1)
-        when 'wl-' then hc.attr_change(:wl, -1)
-        else
-      end
+      puts 'Secondary attributes: <hp>, <wl>, <per>. <q> to exit'
+      input = gets.chop
+      attr_dir =
+          case input
+            when 'q' then continue = false; nil
+            when /^hp\+/ then { attr: :hp }
+            when /^hp\-/ then { attr: :hp }
+            when /^wl\+/ then { attr: :wl }
+            when /^wl\-/ then { attr: :wl }
+            when /^per\+/ then { attr: :per, len: 3 }
+            when /^per\-/ then { attr: :per, len: 3 }
+            else
+          end
+      attr_change(hc, input, attr_dir) unless attr_dir.nil?
     end
   end
 
--- hero_creation.rb
+++ hero_creation.rb
@@ -5,7 +5,7 @@ class HeroCreation
 
   def initialize(template)
     @template = template
-    @tunings = {st: 0, dx: 0, iq: 0, ht: 0, hp: 0, wl: 0}
+    @tunings = {st: 0, dx: 0, iq: 0, ht: 0, hp: 0, wl: 0, per: 0}
     @points = powerlevel
   end
 
@@ -24,20 +24,33 @@ class HeroCreation
         calculate_stat(:st) + @tunings[:hp]
       when :wl
         calculate_stat(:iq) + @tunings[:wl]
+      when :per
+        calculate_stat(:iq) + @tunings[:per]
       else
     end
   end
 
+  def verify_stat(attr)
+    stat_value = calculate_stat(attr)
+    case
+      when (attr == :wl || attr == :per) && (stat_value > 20 || stat_value < 4)
+        result = false
+      else
+        result = true
+    end
+    result
+  end
+
   def calculate_points
     result = powerlevel
-    result -= (@tunings[:st]+@tunings[:ht])*10 + (@tunings[:dx]+@tunings[:iq])*20
-              + @tunings[:hp] * 2 + @tunings[:wl] * 5
+    result -= (@tunings[:st] + @tunings[:ht]) * 10 + (@tunings[:dx] + @tunings[:iq]) * 20 \
+              + @tunings[:hp] * 2 + @tunings[:wl] * 5 + @tunings[:per] * 5
     @points = result
   end
 
   def effective_stats
     result = {}
-    [:st, :dx, :ht, :iq, :bl, :hp, :wl].each { |stat|
+    [:st, :dx, :ht, :iq, :bl, :hp, :wl, :per].each { |stat|
       result[stat] = calculate_stat(stat)
     }
     result
@@ -45,7 +58,11 @@ class HeroCreation
 
   def attr_change(attr, levels)
     @tunings[attr] += levels
-    calculate_points
+    if verify_stat(attr)
+      calculate_points
+    else
+      @tunings[attr] -= levels
+    end
   end
 
 end
